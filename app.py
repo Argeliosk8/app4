37fc323059c89733c04974525409de02a3bfdd86
Argeliosk8 <argelio.baca@gmail.com>
2023-02-15 15:08:30 -0600

    updating profile

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_required, login_user
-
+from os import environ
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'My_secret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or 'sqlite:///myDB.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.app_context().push()
 
 db = SQLAlchemy(app)
-
-
-
 
 
 @app.route('/home')
